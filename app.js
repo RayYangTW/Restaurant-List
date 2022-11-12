c1a18eb15c855b6811efbeacb1777d8e78f31f6b
ray199229 <ray199229@gmail.com>
2022-11-12 17:16:33 +0800

    feat: add show page

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose') // require mongoose
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
+const Restaurant = require('./models/restaurant')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
@@ -22,6 +24,10 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+//setting static files
+app.use(express.static('public'))
+//用 app.use 規定每一筆請求都會透過 body-parser 進行前置處理
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // require json files
 const restaurantList = require('./restaurant.json').results
@@ -30,19 +36,22 @@ const restaurantList = require('./restaurant.json').results
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
-//setting static files
-app.use(express.static('public'))
-
 //setting routes index
 app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
+  Restaurant.find()
+    .lean()
+    .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
 })
 
 //setting routes show
 //動態路由
-app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant })
+app.get('/restaurants/:id', (req, res) => {
+  const restaurant_id = req.params.id
+  Restaurant.findById(restaurant_id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
 })
 
 //setting routes search
