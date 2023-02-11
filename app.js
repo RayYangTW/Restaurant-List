8bd5202ad8aee6ed7f3a494cac19b51e984fb860
ray199229 <ray199229@gmail.com>
2023-02-11 20:49:21 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 //require package
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 require('./config/mongoose')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const Restaurant = require('./models/restaurant')
 const routes = require('./routes')
@@ -14,7 +18,11 @@ const port = 3000
 
 
 //用 app.use 規定每一筆請求都會透過 midle ware 進行前置處理
-
+app.use(session({
+  secret: 'thisIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static('public'))//setting static files
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
