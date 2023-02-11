b451421a6e709d10ca0b65f0581ab5c5c1d7a6dd
ray199229 <ray199229@gmail.com>
2023-02-11 20:26:25 +0800

    feat: add login and register page

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,9 @@ const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
 const users = require('./modules/users')
 
-router.use('/', home)
 router.use('/restaurants', restaurants)
 router.use('/users', users)
+router.use('/', home)
 
 // 匯出路由器
 module.exports = router
